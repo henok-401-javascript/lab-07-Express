af78fdf36271ba7cc085ef6b0c699ed023aa6092
henok Gebremedhn <henoklwi@gmail.com>
2020-04-10 00:00:41 -0700

    finished old my route and mongodb

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 'use strict';
 
 const app = require('./lib/server.js');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const MONGODB_URL = process.env.MONGODB_URL;
+mongoose.connect(MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once('open', function callback() {
+  console.log('Connected to DB!');
+});
 
 let port = process.env.PORT || 3000;
 
-app.start(port);
+app.start(port, MONGODB_URL);
+// mongoose.disconnect();
